ee0ef51347751423cfa167705dac41434e2e8db0
juliette <juliette@MacBook-Air-de-cemos.local>
2026-05-22 15:56:09 +0200

    implement method

--- src/main/java/org/example/DiceScore.java
+++ src/main/java/org/example/DiceScore.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class DiceScore {
+
+    private final Ide de;
+
+    public DiceScore(Ide de) {
+        this.de = de;
+    }
+
+    public int getScore() {
+        int firstScore = de.getRoll();
+        int secondScore = de.getRoll();
+
+        if(firstScore == secondScore) {
+            if(firstScore == 6) {
+                return 30;
+            }
+
+            return firstScore * 2 + 10;
+        }
+
+        return Math.max(firstScore, secondScore);
+    }
 }
